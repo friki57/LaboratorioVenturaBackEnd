960e003c2ff0483abf8d23874d5a55d0b4b6cdbf
Erick Aranibar <aranibarerick@gmail.com>
2023-01-17 22:31:07 -0400

    modificar laboratorio

--- src/Modelo/Examen.js
+++ src/Modelo/Examen.js
@@ -6,6 +6,7 @@ const CamposSchema = new Schema(
         Nombre: String,
         ValorReferencia: String,
         Concentracion: String,
+        SubCategoria: String
     }
 )
 const ExamenSchema = new Schema(
@@ -13,7 +14,6 @@ const ExamenSchema = new Schema(
         Nombre: String,
         Estado: Number,
         Categoria: String,
-        SubCategoria: String,
         Metodo: String,
         Campos: [CamposSchema]
     }

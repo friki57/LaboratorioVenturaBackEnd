213ecc3ddbc29ceef5ba3c54a1c16bdd2ff0d1de
Erick Aranibar <aranibarerick@gmail.com>
2023-01-18 20:44:28 -0400

    modificar laboratorio

--- src/Utils/filtrar.js
+++ src/Utils/filtrar.js
@@ -1,9 +1,33 @@
 export function filtrarLaboratorios(datos, filtro)
 {
     datos = ordenar(datos, filtro.ord)
+    datos = filtrar(datos, filtro);
     return datos;
 }
 
+function filtrar(datos, filtros)
+{
+    if(filtros.Nombre != ""){
+        datos = datos.filter(lab => {
+            console.log(lab.Paciente._doc.NombreCompleto)
+            if (lab.Paciente._doc.NombreCompleto != undefined)
+                return lab.Paciente._doc.NombreCompleto.includes(filtros.Nombre)
+        });
+    }
+    if(filtros.CI != ""){
+        datos = datos.filter(lab => {
+            if (lab.Paciente._doc.CI != undefined)
+                return lab.Paciente._doc.CI.substring(0, filtros.CI.length).toLowerCase() == (filtros.CI).toLowerCase();
+        });
+    }
+    if(filtros.CodigoPaciente != ""){
+        datos = datos.filter(lab => {
+            if (lab.Paciente._doc.CodigoPaciente != undefined)
+                return lab.Paciente._doc.CodigoPaciente.substring(0, filtros.CodigoPaciente.length).toLowerCase() == (filtros.CodigoPaciente).toLowerCase();
+        });
+    }
+    return datos;
+}
 function ordenar(datos, ord)
 {
     switch(ord)

7bd0f294c60ffafdc0d95fb8075ce0ca125e6c13
Erick Aranibar <aranibarerick@gmail.com>
2023-01-19 22:07:48 -0400

    reportes

--- src/Controlador/HTTP/reqLaboratorio.js
+++ src/Controlador/HTTP/reqLaboratorio.js
@@ -142,6 +142,7 @@ export default (rutas) => {
                     let Examenes = a.ExamenesRealizados.map(a=>a.Examen._doc)
                     Examenes = a.ExamenesRealizados.map((a,i)=>{return {...Examenes[i], Resultados: a.Resultados}})
                     console.log(Examenes)
+                    a.ExamenesRealizados = Examenes
                     res.json(a)
                     console.log("******************** Fin Leer Uno Laboratorio ********************");
                 })

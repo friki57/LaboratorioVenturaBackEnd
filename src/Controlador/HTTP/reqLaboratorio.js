c3ce2e955d8999284a7075347184274328d8c73c
Erick Aranibar <aranibarerick@gmail.com>
2023-01-21 15:45:45 -0400

    reportes

--- src/Controlador/HTTP/reqLaboratorio.js
+++ src/Controlador/HTTP/reqLaboratorio.js
@@ -255,7 +255,7 @@ export default (rutas) => {
                     a.ExamenesFinal = examf
                     a.Paciente = a.Paciente._doc
                     a.Paciente.Edad = calcularEdad(a.Paciente.Fecha_de_Nacimiento)
-                    //console.log(a)
+                    console.log(JSON.stringify(a, null, 4))
                     reporteLaboratorio(a, ()=>
                     {
                         res.download("./src/Utils/ReporteLaboratorio.docx")

bfbf22d53ad046e237d28921f5b1ded52be4546c
Erick Aranibar <aranibarerick@gmail.com>
2023-01-26 22:26:59 -0400

    reportes

--- src/Controlador/HTTP/reqLaboratorio.js
+++ src/Controlador/HTTP/reqLaboratorio.js
@@ -22,6 +22,7 @@ export default (rutas) => {
                                 CodigoPaciente: "0000",
                                 CI: "0",
                                 Nombres: "Desconocido o eliminado",
+                                NombreCompleto: "Desconocido o eliminado",
                                 PrimerApellido: "",
                                 SegundoApellido: "",
                                 Fecha_de_Nacimiento: "01-01-2023",

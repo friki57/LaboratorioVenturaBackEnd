0c242535ed3ea7e4959e35946e953325c54e33b7
friki57 <48742935+friki57@users.noreply.github.com>
2023-01-31 15:15:49 -0400

    filtrar error

--- src/Utils/filtrar.js
+++ src/Utils/filtrar.js
@@ -11,7 +11,7 @@ function filtrarLaboratorio(datos, filtros)
     console.log(datos, filtros)
     if (filtros.Estado != "" && filtros.Estado != "Todos") {
         datos = datos.filter(lab => {
-            if (lab.Paciente._doc.NombreCompleto != undefined){
+            if (lab.Paciente.NombreCompleto != undefined){
                 if(filtros.Estado=="Pendiente")
                 return lab.ExamenesRealizados.filter(ex=>ex.Estado==filtros.Estado).length>0
                 if(filtros.Estado=="Realizado"){
@@ -23,21 +23,21 @@ function filtrarLaboratorio(datos, filtros)
     }
     if(filtros.Nombre != ""){
         datos = datos.filter(lab => {
-            if (lab.Paciente._doc.NombreCompleto != undefined)
-                return lab.Paciente._doc.NombreCompleto.substring(0, filtros.Nombre.length).toLowerCase() == (filtros.Nombre).toLowerCase()
-            // return lab.Paciente._doc.NombreCompleto.includes(filtros.Nombre)
+            if (lab.Paciente.NombreCompleto != undefined)
+                return lab.Paciente.NombreCompleto.substring(0, filtros.Nombre.length).toLowerCase() == (filtros.Nombre).toLowerCase()
+            // return lab.Paciente.NombreCompleto.includes(filtros.Nombre)
         });
     }
     if(filtros.CI != ""){
         datos = datos.filter(lab => {
-            if (lab.Paciente._doc.CI != undefined)
-                return lab.Paciente._doc.CI.substring(0, filtros.CI.length).toLowerCase() == (filtros.CI).toLowerCase();
+            if (lab.Paciente.CI != undefined)
+                return lab.Paciente.CI.substring(0, filtros.CI.length).toLowerCase() == (filtros.CI).toLowerCase();
         });
     }
     if(filtros.CodigoPaciente != ""){
         datos = datos.filter(lab => {
-            if (lab.Paciente._doc.CodigoPaciente != undefined)
-                return lab.Paciente._doc.CodigoPaciente.substring(0, filtros.CodigoPaciente.length).toLowerCase() == (filtros.CodigoPaciente).toLowerCase();
+            if (lab.Paciente.CodigoPaciente != undefined)
+                return lab.Paciente.CodigoPaciente.substring(0, filtros.CodigoPaciente.length).toLowerCase() == (filtros.CodigoPaciente).toLowerCase();
         });
     }
     return datos;

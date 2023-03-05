b220cfd068d75356858a01bf9e728f2e5796d80f
Erick Aranibar <aranibarerick@gmail.com>
2023-03-05 17:38:14 -0400

    arreglar editar laboratorio

--- src/Controlador/HTTP/reqLaboratorio.js
+++ src/Controlador/HTTP/reqLaboratorio.js
@@ -73,6 +73,7 @@ export default (rutas) => {
             crudExamen.buscarTodo((examenes)=>
             {
                 crudLaboratorio.buscarUno(id, (laboratorio) => {
+                    console.log(laboratorio, id)
                     let a = laboratorio._doc;
                     a.Paciente = pacientes.find(b=>b._id == a.IdPaciente)
                     a.Paciente = a.Paciente._doc;

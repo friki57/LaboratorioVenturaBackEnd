6e9bb0c69934e1a7b609c24bd105e7952dbb4a1a
NUEVATEL\eraranibar <erick.aranibar@nuevatel.com>
2024-02-05 11:29:35 -0400

    login

--- src/Controlador/Cruds/crudUsuario.js
+++ src/Controlador/Cruds/crudUsuario.js
@@ -16,6 +16,7 @@ function crud()
     {
         Usuario.find((err, users)=>
         {
+            console.log(users)
             const user = users.find(us=>us._doc.Email === correo);
             if(!err) callback(user)
             else console.log(err)

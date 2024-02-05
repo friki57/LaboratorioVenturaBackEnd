d15d35c8ec92b77b637048ffcd055afa1a5e964b
NUEVATEL\eraranibar <erick.aranibar@nuevatel.com>
2024-02-05 10:58:22 -0400

    login

--- src/Controlador/HTTP/reqLogin.js
+++ src/Controlador/HTTP/reqLogin.js
@@ -2,6 +2,7 @@ import { encriptarContra, desencriptarContra } from "../../Utils/encriptacion.js
 import crudUsuario from "../Cruds/crudUsuario.js";
 import { calcularEdad } from "../../Utils/calcEdad.js";
 import { filtrarPacientes } from "../../Utils/filtrar.js";
+import passport from 'passport';
 
 export default (rutas) => {
     rutas.post("/login", passport.authenticate("iniciar sesion",

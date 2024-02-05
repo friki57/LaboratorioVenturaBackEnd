72166c667d719ee1e6c419bd6b8d60604fd3488e
NUEVATEL\eraranibar <erick.aranibar@nuevatel.com>
2024-02-05 11:35:36 -0400

    login

--- src/Controlador/HTTP/reqLaboratorio.js
+++ src/Controlador/HTTP/reqLaboratorio.js
@@ -21,7 +21,7 @@ export default (rutas) => {
         console.log("******************** Leer Cantidad Laboratorio ********************\n");
         crudLaboratorio.buscarTodo((laboratorios) => {
             let ret = laboratorios.map(a=>a._doc);
-            res.json({ cant: ret.length, user: req.user })
+            res.json({ cant: ret.length, user: req.user, cos: 'a' })
             console.log("******************** Fin Leer Cantidad Laboratorio ********************");
         })
     });

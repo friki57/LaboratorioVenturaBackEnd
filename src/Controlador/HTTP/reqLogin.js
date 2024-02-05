cf6c0a8d9c89871a34864fb1bc070ce626e6a0c0
NUEVATEL\eraranibar <erick.aranibar@nuevatel.com>
2024-02-05 11:37:48 -0400

    login

--- src/Controlador/HTTP/reqLogin.js
+++ src/Controlador/HTTP/reqLogin.js
@@ -15,4 +15,13 @@ export default (rutas) => {
             res.json(req.user);
         }
     );
+    rutas.post('/logout', async (req, res, next) => {
+        try {
+            await req.session.destroy();
+        } catch (err) {
+            console.error('Error logging out:', err);
+            return next(new Error('Error logging out'));
+        }
+        res.status(200).send();
+    })
 }
